5ebf3d5c21326308ab93eb065e1406352cb33e2e
salehDf <appsvg2@gmail.com>
2016-04-13 14:53:54 +0430

    Create hlep.lua

---
+++ plugins/hlep.lua
@@ -0,0 +1,21 @@
+do
+
+function run(msg, matches)
+  return [[
+
+متن راهنما
+
+]]
+end
+
+return {
+  description = "نمایش راهنمای ربات",
+  usage = "نمایش راهنمای ربات",
+  patterns = {
+    "^Help$",
+    "^help$"
+  },
+  run = run
+}
+
+end
